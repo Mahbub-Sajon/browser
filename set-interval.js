788d1d65bce5d664a4754256a51f779c7ec84c8c
Mahbub-Sajon <sajon.mahbub@gmail.com>
2022-04-21 01:46:19 +0600

    first commit

---
+++ set-interval.js
@@ -0,0 +1,13 @@
+console.log('firts');
+// setInterval(() =>{
+//     console.log('Third');
+// }, 3000);
+let seconds = 0;
+const timeId = setInterval(()=>{
+    seconds++;
+    console.log(seconds);
+    if(seconds >= 15){
+        clearInterval(timeId);
+    }
+},1000);
+console.log('second');
---
+++ time-out.js
@@ -0,0 +1,8 @@
+console.log("this is the first line");
+setTimeout(function (){
+    console.log('I am using vs code')
+});
+setTimeout(() => {
+    console.log('Exploring something')
+}, 4000);
+console.log('hello')
